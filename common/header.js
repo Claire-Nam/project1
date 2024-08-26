7214b3d18a51e8d03f138088ec342a6dc9459923
Claire <nbr1023@naver.com>
2024-08-26 14:32:03 +0900

    공통 사항 컴포넌트화 및 페이지별 적용

---
+++ common/header.js
@@ -0,0 +1,12 @@
+window.__globalHeader = `
+    <header role="header" id="header">
+        <div class="head-container">
+            <div class="logo">
+                    <img src="image/korea_culture_logo.jpg" alt="로고">
+            </div>
+            <div class="search-bar">
+                <input type="text" placeholder="검색어를 입력해주세요">
+                    <img src="image/search.png" alt="검색">
+            </div>
+        </div>
+    </header>`
---
+++ common/nav.js
@@ -0,0 +1,16 @@
+window.__globalNav = `
+ <footer id="footer">
+        <div class="nav-bar">
+            <ul>
+                <li><a href="train.html">
+                    <img src="image/house.svg" alt="home"> <br />
+                    Home</a></li>
+                <li><a href="restaurant.html">
+                    <img src="image/cup-hot.svg" alt="foods"> <br />Foods</a></li>
+                <li><a href="festival.html">
+                    <img src="image/flower2.svg" alt="festival"> <br />Travel</a></li>
+                <li><a href="park.html">
+                    <img src="image/p-square.svg" alt="park"> <br />Parking</a></li>
+            </ul>
+        </div>
+    </footer>`;
