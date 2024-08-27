597825df31495096bfc6147af3075524dd1bc448
Claire <nbr1023@naver.com>
2024-08-27 14:13:13 +0900

    캐러셀 생성중

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-const search = document.querySelector('input');
+document.addEventListener('DOMContentLoaded', function() {
+    const prevButton = document.querySelector('.swiper-btn-prev');
+    const nextButton = document.querySelector('.swiper-btn-next');
+    const slides = document.querySelectorAll('.swiper-slide');
+    let currentIndex = 0;
 
-function search() {
-    if(search == '') {
-        return alert("검색어를 입력해주세요")
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.style.display = i === index ? 'block' : 'none';
+        });
     }
-}
+
+    function prevSlide() {
+        currendIndex = (currentIndex > 0) ? currentIndex -1 : slides.length -1;
+        showSlide(currentIndex);
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex < slides.length -1) ? currentIndex + 1 : 0;
+        showSlide(currentIndex);
+    }
+
+    prevButton.addEventListener('click', prevSlide);
+    nextButton.addEventListener('click', nextSlide);
+
+    //초기 이미지 표시
+    showSlide(currentIndex);
+});
