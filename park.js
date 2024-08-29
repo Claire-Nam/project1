548f26873bccf2da6e9ebc6036aa67118ab2d26b
Claire <nbr1023@naver.com>
2024-08-29 15:55:37 +0900

    네이버 지도 API 연동 수정중

--- park.js
+++ park.js
@@ -1,6 +1,9 @@
-const mapOptions = {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 10
-};
+var map = new naver.maps.Map("map", {
+  center: new naver.maps.LatLng(37.5112, 127.0981), // 잠실 롯데월드를 중심으로 하는 지도
+  zoom: 15,
+});
 
-const map = new naver.maps.Map('map', mapOptions);
+var marker = new naver.maps.Marker({
+  position: new naver.maps.LatLng(37.5112, 127.0981),
+  map: map,
+});
